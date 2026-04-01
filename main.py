98fe82cced3972c6551553c9aed205857ccd98d9
Jaynth v <135266258+Jaynth-v@users.noreply.github.com>
2026-04-01 08:35:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import requests
 CONFIG = {
     "url": os.getenv(
         "BMS_URL",
-        "https://in.bookmyshow.com/movies/chennai/dhurandhar-the-revenge/buytickets/ET00478890"
+        "https://in.bookmyshow.com/movies/bengaluru/project-hail-mary/buytickets/ET00481564/20260401"
     ),
     "dates": os.getenv("BMS_DATES", ""),          # comma-separated YYYYMMDD, empty = from URL
     "theatre": os.getenv("BMS_THEATRE", ""),      # substring filter, empty = all
